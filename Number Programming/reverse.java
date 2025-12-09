1162d26cfb7063bed1fa8b13f20c47351ebaa1d3
Darshanasri Sasikumar <darshanasrits@gmail.com>
2025-12-09 21:11:31 +0530

    changes

---
+++ Number Programming/reverse.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class reverse 
+{
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        int rev = 0;
+
+        while (num > 0) {
+            int digit = num % 10;        // take last digit
+            rev = rev * 10 + digit;      // add digit to reverse number
+            num = num / 10;              // remove last digit
+        }
+
+        System.out.println("Reversed Number: " + rev);
+    }
+}
