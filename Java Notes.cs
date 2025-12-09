c8eb47b91634ba953bc4b46a383ebd548f927081
Darshana <darshana@cloudrevelinnovation.com>
2025-12-09 16:36:45 +0530

    Correction

---
+++ Java Notes.cs
@@ -0,0 +1,82 @@
+Java Features
+1.Java is platform Independent Programming Languvange
+2.Simple and Secured in Nature 
+3.It is an Object Oriented Programming Languvange
+4.Java Supports Muliti-Threading
+5.Robust in Nature((strong typing, exception handling, automatic memory))
+
+why Java is Platform Independent Languvange
+1.Java Supports WORA(Write Once and Run AnyWhere)
+2.Any Program that is Return in Java can be executed in any Platform
+3.Java is Independent Languvange but Jvm is dependent One
+4.Here Executable file is .class which contains byte code
+
+Compiler 
+1.Compiler work is to check Java code and then Conver it into byte Code
+2.It is used to List Syntax Mistakes
+3.The compiler (javac) converts it into a .class file that contains bytecode.(bytecode is nothing but a special intermediate code)
+4.It checks the entire program before running it.
+5.It can make a program Compile Time Sucess (CTS) and Compile time Error (CTE)
+
+Interpreter
+1.JVM is Interpreter
+2.It Converts Bite Code into Binary Code
+
+JDK(Java Development Kit)
+1.It contains two Important Documents
+2.It contains TWO Important Components ie.. Development Tools and JRE(Java Run Time Environment)
+3.JVM is Present inside JRE
+4.JVM Job is to Convert Byte Code into binary
+
+Commands
+Compilation command-->javac FileName.java
+Execution command-->java ClassName
+
+Tokens:
+In Java, tokens are the smallest meaningful units of a program.
+There are 5 types of Tokens:
+          1.Keywords
+          2.identifier
+          3.Operators
+          4.literals
+          5.special symbols
+
+
+Keywords
+1.keywords are reserved words
+2.It is Predefined Words and also called as reserved Word
+3.We have 49+ keywords
+4.some of them are public,static,int,double,void,final,abstract etc.....
+
+identifier
+1.In java we are having components such as class,variable,methods ,interface called as identifiers
+2.it is used for identifyin class,method,variable or interface
+Rules of Identifeirs:
+    1.Can't Start with Numbers But we can Have Numbers inbetween or Last
+    2.Except $ and _ can't use other Special Characters
+    3.Wide spaces or not allowd
+    4.Keywords can't be named for Identifeirs
+
+Operators
+1.it is Pre-defined symbols to perform Operations
+2.List of Operators
+  1.Arithmatic Operators
+  2.TypeCast Operators
+  3.Relational Operator
+  4.Logical Operators
+  5.Conditional Operators
+  6.Increament and Decreament Operator
+  7.Compound Assignment Operator
+
+literals
+1.Literals are nothing but Fixed values that can be written in java programming Languvange
+2.The literal is the actual value stored inside the variable
+
+
+
+
+
+
+
+
+
