585b11e9e460265b053dfdf6d9e6d2626fa93fbc
Darshana <darshana@cloudrevelinnovation.com>
2025-12-09 15:58:47 +0530

    Even and Odd Programing

---
+++ evenorodd.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class evenorodd{
+    public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a Number");
+        int c = sc.nextInt();
+        if(c%2 == 0){
+            System.out.println("Its even");
+        }else {
+            System.out.println("Its odd");
+
+        }
+
+
+
+
+
+//---------------------------------------------------------------------------------------------------------------------------------------------------------------------
+        int a =10;
+        if(a%2 == 0){
+            System.out.println("It is even ");
+
+        }else {
+            System.out.println("It is odd ");
+
+        }
+    }
+}
---
+++ swap.java
@@ -0,0 +1,38 @@
+public class swap {
+
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+
+        // ---------------------------------------------------------------------------------------------------------------------------------------------
+        // using 3 variables
+        System.out.println("The first Number is " + a);
+        System.out.println("The Secound Number is  " + b);
+        int temp = a;
+        a = b;
+        b = temp;
+
+        System.out.println("The swap first number is " + a);
+        System.out.println("The swap Secounf number is " + b);
+        // ----------------------------------------------------------------------------------------------------------------------------------------------------------
+        // using Addition and Subtraction
+
+        a = a + b;
+        b = a - b;
+        a = a - b;
+        System.out.println("The swap first number is " + a);
+        System.out.println("The swap Secounf number is " + b);
+
+        // ---------------------------------------------------------------------------------------------------------------------------------------------------------
+
+        // using Multiplication and Division
+        a = a * b;
+        b = a / b;
+        a = a / b;
+
+        System.out.println("The swap first number is " + a);
+        System.out.println("The swap Secounf number is " + b);
+
+    }
+
+}
